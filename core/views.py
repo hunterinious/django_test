c5b27436a24edf03de71f40e6d0fae790e9582a8
Vladyslav Nerodenko <vladyslavnerodenko@Vladyslavs-MacBook-Pro.local>
2021-03-19 15:12:54 +0200

    admin obly view moved to core

--- core/views.py
+++ core/views.py
@@ -1,2 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views import View
 
+
+class AdminOnlyView(LoginRequiredMixin, UserPassesTestMixin, View):
+    permission_denied_message = 'Only admin has access to this view'
+
+    def test_func(self):
+        return self.request.user.is_superuser
--- posts/views.py
+++ posts/views.py
@@ -1,23 +1,16 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (
     CreateView,
     ListView,
     UpdateView,
     DeleteView
 )
-from django.views import View
 from django.core.exceptions import PermissionDenied
 from django.urls import reverse
 from django.shortcuts import get_object_or_404
 from .forms import PostForm
 from .models import Post
-
-
-class AdminOnlyView(LoginRequiredMixin, UserPassesTestMixin, View):
-    permission_denied_message = 'Only admin has access to this view'
-
-    def test_func(self):
-        return self.request.user.is_superuser
+from core.views import AdminOnlyView
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
